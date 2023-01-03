30882ecf8b0a8f0805a1a85265ed0f837925f7db
Emmanuel <Egelaonufriday@gmail.com>
2023-01-03 10:27:04 -0800

    fixed strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,8 +13,8 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; *(s + 1); i++)
-	{
+	for (i = 0; *(s + i); i++)
+i	{
 		for (j = 0; *(accept + j); j++)
 		{
 			if (*(s + i) == *(accept + j))
